59a122558d47bb5e4605697f9e16ee8d84b25aef
EternallyAscend <52477444+EternallyAscend@users.noreply.github.com>
2020-04-03 16:53:43 +0800

    Create Suffer From Myself.go

---
+++ Composition/Lyrics/Temp/Suffer From Myself.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+
+fmt.Println("_________________________")
+fmt.Println("Suffer From Myself (2020)")
+fmt.Println("Singer: Tom")
+fmt.Println("Songwriting: Tom")
+fmt.Println("Arranger: Tom")
+fmt.Println(""+
+""+
+""+
+"远行的人 你的背影已湮灭"+
+"忘却的是你沧海遗珠的回眸"+
+"忘却的是我自以为是的拥有"+
+"忘却的是那子虚乌有的一切"+
+"悄然拟合那曾经传说的预言"+
+""+
+"Music"+
+""+
+"将我的幻想置放在宇宙星辰"+
+"任日月穿梭其间"+
+"直到那万物虚无灰飞又烟灭"+
+"灵魂一去不回还"+
+""+
+"原来是我自作自受逃离伊甸园"+
+"原来是我自作自受逃离伊甸园"+
+"原来是我自作自受逃离伊甸园"+
+"")
